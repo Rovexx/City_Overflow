1ceaadd44045e4d9cc368bbbe2ad22d9fa25ea94
Rovexx <roel.versteeg@hotmail.com>
2020-10-16 15:34:41 +0200

    Added simple water pump

---
+++ City Overflow/Assets/Scripts/PumpController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpController : MonoBehaviour
+{
+    public float pumpSpeed;
+    public bool powered;
+    public Transform rotor;
+
+    void FixedUpdate()
+    {
+        if (powered)
+        {
+            rotor.Rotate (pumpSpeed, 0, 0 * Time.fixedDeltaTime);
+        }
+    }
+}
